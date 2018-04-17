61d1dced818871d60e8dd001374d69d8fb699989
Rainer Pons <rpons@ucsc.edu>
2018-04-17 15:57:20 -0700

    Update with Lombok
    Updated the Clothing model and pom.xml with Lombok to reduce amount of
    boilerplate code.

--- src/main/java/com/rainerpons/pocketcloset/model/Clothing.java
+++ src/main/java/com/rainerpons/pocketcloset/model/Clothing.java
@@ -2,6 +2,11 @@ package com.rainerpons.pocketcloset.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Document(collection = "clothing")
 public class Clothing {
 	private String id;
@@ -12,45 +17,5 @@ public class Clothing {
 
 	public Clothing() {
 		// Do nothing because of setter-based dependency injection.
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
 	}
 }
