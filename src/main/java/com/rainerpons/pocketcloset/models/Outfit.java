a80ed131e5d520bc0ea01b554833eade0c1eb2ef
Rainer Pons <rpons@ucsc.edu>
2018-04-24 21:22:20 -0700

    Create outfit model
    Created the Outfit model which contains some properties, including a
    list of Clothing objects

---
+++ src/main/java/com/rainerpons/pocketcloset/models/Outfit.java
@@ -0,0 +1,24 @@
+package com.rainerpons.pocketcloset.models;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Document(collection = "outfits")
+public class Outfit {
+	private String id;
+	private String description;
+	private List<Clothing> tops;
+	private List<Clothing> bottoms;
+	private List<Clothing> footwear;
+	private List<Clothing> accessories;
+	
+	public Outfit() {
+		// Do nothing because of setter-based dependency injection.
+	}
+}
